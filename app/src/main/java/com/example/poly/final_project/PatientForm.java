d761a366b49f4a33c9cf17c472dd0e323f06a281
Zeel <zeeldiyora2@gmail.com>
2018-04-18 09:16:28 -0400

    Zeel: Half Done 2 Page

--- app/src/main/java/com/example/poly/final_project/PatientForm.java
+++ app/src/main/java/com/example/poly/final_project/PatientForm.java
@@ -1,13 +1,37 @@
 package com.example.poly.final_project;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PatientForm extends Activity {
+
+    protected static final String ACTIVITY_NAME = "PatientForm";
+    Button patBackBtn;
+    Button patSaveBtn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_form);
+
+        patBackBtn = (Button) findViewById(R.id.patBack);
+        patSaveBtn = (Button) findViewById(R.id.patSave);
+
+        patBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(PatientForm.this, ClinicActivity.class);
+                startActivityForResult(intent, 50);
+            }
+        });
+
+
+
+
     }
 }
